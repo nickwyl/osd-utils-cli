01a7eee665ea88cf634a1b990e813424df1e2a07
Nick Wang <ylw238@nyu.edu>
2020-07-30 11:53:55 -0400

    Adding -t 3M option

--- cmd/cost/create_test.go
+++ cmd/cost/create_test.go
@@ -1,20 +1,19 @@
 package cost
 
 import (
-	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/organizations"
+	"github.com/aws/aws-sdk-go/service/costexplorer"
 	"github.com/golang/mock/gomock"
-	. "github.com/onsi/gomega"
 	"github.com/openshift/osd-utils-cli/pkg/provider/aws/mock"
+	//"gotest.tools/assert"
 	"testing"
+	gomega "github.com/onsi/gomega"
 )
 
 type mockSuite struct {
 	mockCtrl      *gomock.Controller
-	mockOrgClient *mock.MockOrganizationsClient
-	mockCEClient  *mock.MockCostExplorerClient
+	mockAwsClient *mock.MockClient
 }
 
 func setupAWSMocks(t *testing.T) *mockSuite {
@@ -22,72 +21,117 @@ func setupAWSMocks(t *testing.T) *mockSuite {
 		mockCtrl: gomock.NewController(t),
 	}
 
-	mocks.mockOrgClient = mock.NewMockOrganizationsClient(mocks.mockCtrl)
-	mocks.mockCEClient = mock.NewMockCostExplorerClient(mocks.mockCtrl)
+	mocks.mockAwsClient = mock.NewMockClient(mocks.mockCtrl)
 
 	return mocks
 }
 
 func TestCreateCostCategory(t *testing.T) {
-	g := NewGomegaWithT(t)
-	testCases := []struct {
-		title	     string
-		OUid         *string
-		OU           *organizations.OrganizationalUnit
-		setupOrgMock func (r *mock.MockOrganizationsClientMockRecorder)
-		setupCEMock  func(r *mock.MockCostExplorerClientMockRecorder)
-		errExpected  bool
-	}{
-		{
-			title: "Error calling CreateCostCategoryDefinition API",
-			OUid: aws.String("ou-0wd6-oq5d7v8g"),
-			OU: &organizations.OrganizationalUnit{Id: aws.String("ou-0wd6-oq5d7v8g")},
-			setupOrgMock: func(d *mock.MockOrganizationsClientMockRecorder) {
-				d.ListOrganizationalUnitsForParent(gomock.Any()).
-					Return(nil).Times(1)
+	//g := NewGomegaWithT(t)
+	//testCases := []struct {
+	//	title	     string
+	//	OUid         *string
+	//	OU           *organizations.OrganizationalUnit
+	//	setupAwsClientMock func (r *mock.MockClientMockRecorder)
+	//	errExpected  bool
+	//}{
+	//	{
+	//		title: "Error calling CreateCostCategoryDefinition API",
+	//		OUid: aws.String("ou-0wd6-oq5d7v8g"),
+	//		OU: &organizations.OrganizationalUnit{Id: aws.String("ou-0wd6-oq5d7v8g")},
+	//		setupAwsClientMock: func(r *mock.MockClientMockRecorder) {
+	//			r.CreateCostCategoryDefinition(gomock.Any()).
+	//				Return(nil, errors.New("FakeError")).Times(1)
+	//			r.ListOrganizationalUnitsForParent(gomock.Any()).
+	//				Return(nil, nil).Times(1)
+	//		},
+	//		errExpected: true,
+	//	},
+	//	{
+	//		title: "Cost category already exists",
+	//		OUid: aws.String("ou-0wd6-oq5d7v8g"),
+	//		OU: &organizations.OrganizationalUnit{Id: aws.String("ou-0wd6-oq5d7v8g")},
+	//		setupAwsClientMock: func(r *mock.MockClientMockRecorder) {
+	//			r.CreateCostCategoryDefinition(gomock.Any()).
+	//				Return(nil, awserr.New(
+	//					"ValidationException",
+	//					"Failed to create Cost Category: Cost category name already exists",
+	//					nil,
+	//					)).Times(1)
+	//			r.ListOrganizationalUnitsForParent(gomock.Any()).
+	//				Return(nil, nil).Times(1)
+	//		},
+	//		errExpected: true,
+	//	},
+	//}
+
+	//for _, tc := range testCases {
+	//	t.Run(tc.title, func(t *testing.T) {
+	//		mocks := setupAWSMocks(t)
+	//
+	//		// This is necessary for the mocks to report failures like methods not being called an expected number of times.
+	//		// after mocks is defined
+	//		defer mocks.mockCtrl.Finish()
+	//
+	//		//OU := &organizations.OrganizationalUnit{Id: aws.String("ou-0wd6-oq5d7v8g")}
+	//		//var OUSlice []*organizations.OrganizationalUnit
+	//		//OUSlice = append(OUSlice, OU)
+	//		//
+	//		//OUs := &organizations.ListOrganizationalUnitsForParentOutput{
+	//		//	OrganizationalUnits: OUSlice,
+	//		//	NextToken: nil,
+	//		//}
+	//		//
+	//		//
+	//		//mocks.mockAwsClient.EXPECT().ListOrganizationalUnitsForParent(gomock.Any()).Return(OUs, nil).Times(1)
+	//
+	//		err := createCostCategory(tc.OUid, tc.OU, mocks.mockAwsClient)
+	//
+	//		if tc.errExpected {
+	//			g.Expect(err).Should(HaveOccurred())
+	//		} else {
+	//			g.Expect(err).ShouldNot(HaveOccurred())
+	//		}
+	//	})
+	//}
+
+
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	awsReceiver := mock.NewMockClient(controller)
+
+	input := &costexplorer.CreateCostCategoryDefinitionInput{
+		Name:        aws.String("ou-0wd6-0huixd5v"),
+		RuleVersion: aws.String("CostCategoryExpression.v1"),
+		Rules: []*costexplorer.CostCategoryRule{
+			{
+				Rule: &costexplorer.Expression{
+					Dimensions: &costexplorer.DimensionValues{
+						Key:    aws.String("LINKED_ACCOUNT"),
+						Values: []*string{aws.String("843659886818")},
+					},
+				},
+				Value: aws.String("ou-0wd6-0huixd5v"),
 			},
-			setupCEMock: func(r *mock.MockCostExplorerClientMockRecorder) {
-				r.CreateCostCategoryDefinition(gomock.Any()).
-					Return(nil, errors.New("FakeError")).Times(1)
-			},
-			errExpected: true,
-		},
-		{
-			title: "Cost category already exists",
-			OUid: aws.String("ou-0wd6-oq5d7v8g"),
-			OU: &organizations.OrganizationalUnit{Id: aws.String("ou-0wd6-oq5d7v8g")},
-			setupOrgMock: func(r *mock.MockOrganizationsClientMockRecorder) {
-				r.ListOrganizationalUnitsForParent(gomock.Any()).
-					Return(nil).Times(1)
-			},
-			setupCEMock: func(r *mock.MockCostExplorerClientMockRecorder) {
-				r.CreateCostCategoryDefinition(gomock.Any()).
-					Return(nil, awserr.New(
-						"ValidationException",
-						"Failed to create Cost Category: Cost category name already exists",
-						nil,
-						)).Times(1)
-			},
-			errExpected: true,
 		},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.title, func(t *testing.T) {
-			mocks := setupAWSMocks(t)
+	awsReceiver.EXPECT().CreateCostCategoryDefinition(input).
+		Return(nil, awserr.New(
+			"ValidationException",
+			"Failed to create Cost Category: Cost category name already exists",
+			nil,
+		)).Times(1)
 
-			// This is necessary for the mocks to report failures like methods not being called an expected number of times.
-			// after mocks is defined
-			defer mocks.mockCtrl.Finish()
 
-			//OU := &organizations.OrganizationalUnit{Id: aws.String("ou-0wd6-oq5d7v8g")}
-			err := createCostCategory(tc.OUid, tc.OU, mocks.mockOrgClient, mocks.mockCEClient)
+	awsClient, _ := opsCost.initAWSClients()
+	OU := getOU(awsClient, "ou-0wd6-0huixd5v")
 
-			if tc.errExpected {
-				g.Expect(err).Should(HaveOccurred())
-			} else {
-				g.Expect(err).ShouldNot(HaveOccurred())
-			}
-		})
-	}
+	err := createCostCategory(aws.String("ou-0wd6-0huixd5v"), OU, awsClient)
+
+	g := gomega.NewGomegaWithT(t)
+
+	g.Expect(err)
+
 }
